58db9502f1a3aa9bd47e4385ed3ab90d00f10092
Bianca Ercole <127907362+biancuercole@users.noreply.github.com>
2023-05-17 22:56:40 -0300

    Pantalla de winner y game over terminadas

--- assets/scenes/GameOver.js
+++ assets/scenes/GameOver.js
@@ -3,16 +3,36 @@ export default class GameOver extends Phaser.Scene {
     super("gameOver");
   }
 
-  init() {}
-
-  preload() {}
-
-  create() {
-    this.GOText = this.add.text (400, 300, "Game Over", {
-      fontSize: "20px",
-      fill: "#FAF2F9"
-    })
+  init() {
   }
 
-  update() {}
+
+  preload () {
+  this.load.image("sky", "./assets/images/Cielo.png");
+  this.load.image("platform", "./assets/images/platform.png");
+  this.load.image("player", "./assets/images/Ninja.png");
+  }
+
+  create () {
+      this.add.image(400, 300, "sky").setScale(0.555);
+      this.winText = this.add.text (320, 260, "Perdiste", {
+          fontFamily: "Arial",
+          fontType: "bold",
+          fontSize: "50px",
+          fill: "#E0CDF8",
+          align: "center",
+          backgroundColor: "#8E4EDE",
+      });
+      this.player = this.physics.add.sprite(415, 450, "player");
+      this.platformG = this.physics.add.staticGroup();
+      this.platformG.create(400,580, "platform").setScale(2).refreshBody();
+      this.physics.add.collider(this.platformG, this.player);
+      this.player.setBounce(1);
+      this.player.setVelocityY(-200);
+
+  }
+
+  update () {}
+  
 }
+
--- assets/scenes/Winner.js
+++ assets/scenes/Winner.js
@@ -1,3 +1,4 @@
+import {TRIANGULO, CUADRADO, ROMBO, CRUZ, SHAPES} from "../../utils.js"
 export default class Winner extends Phaser.Scene {
     constructor () {
         super ("Winner");
@@ -10,7 +11,10 @@ export default class Winner extends Phaser.Scene {
     preload () {
     this.load.image("sky", "./assets/images/Cielo.png");
     this.load.image("platform", "./assets/images/platform.png");
-    this.load.image("player", "./assets/images/Ninja.png");
+    this.load.image(TRIANGULO, "./assets/images/Triangulo.png");
+    this.load.image(ROMBO, "./assets/images/Rombo.png");
+    this.load.image(CUADRADO, "./assets/images/cuadrado.png");
+    this.load.image(CRUZ, "./assets/images/Cruz.png");
     }
 
     create () {
@@ -23,16 +27,25 @@ export default class Winner extends Phaser.Scene {
             align: "center",
             backgroundColor: "#8E4EDE",
         });
-        this.player = this.physics.add.sprite(415, 450, "player");
+        this.shapesG = this.physics.add.group();
         this.platformG = this.physics.add.staticGroup();
         this.platformG.create(400,580, "platform").setScale(2).refreshBody();
-        this.physics.add.collider(this.platformG, this.player);
-        this.player.setBounce(1);
-        this.player.setVelocityY(-200);
+        this.physics.add.collider(this.platformG, this.shapesG);
+
+        this.time.addEvent({
+            delay: 500,
+            callback: this.addShape,
+            callbackScope: this,
+            loop: true,
+        });
 
     }
 
     update () {}
-    
+    addShape() {
+        const randomShape = Phaser.Math.RND.pick(SHAPES);
+        const randomX = Phaser.Math.RND.between(0, 800);
+        this.shapesG.create(randomX, 0, randomShape) 
+    }
 }
 
