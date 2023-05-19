6827f04e9172eb831615a43108d7524eb088bd9b
Bianca Ercole <127907362+biancuercole@users.noreply.github.com>
2023-05-19 08:53:12 -0300

    .

--- assets/scenes/Game.js
+++ assets/scenes/Game.js
@@ -68,7 +68,7 @@ export default class Game extends Phaser.Scene {
 
     //evento para agregar la bomba 
     this.time.addEvent ({
-      delay: 10000,
+      delay: 8000,
       callback: this.dropBomb,
       callbackScope: this,
       loop: true,
